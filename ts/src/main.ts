e98e99e735d704667ffd960e9a84cb80f0189229
McPotato <luc.sydney.georges@gmail.com>
2021-09-30 21:46:38 +0200

    got simple kv store working

--- ts/src/main.ts
+++ ts/src/main.ts
@@ -1,27 +1,43 @@
 const IPFS = require('ipfs')
 const OrbitDB = require('orbit-db')
 
+// For js-ipfs >= 0.38
+
+// Create IPFS instance
+const initIPFSInstance = async () => {
+  return await IPFS.create({ repo: "./omniboardio" });
+};
+
+initIPFSInstance().then(async ipfs => {
+  const orbitdb = await OrbitDB.createInstance(ipfs);
+
+  // Create / Open a database
+  const db = await orbitdb.kvstore("main");
+  await db.load();
+
+  // Listen for updates from peers
+  db.events.on("replicated", _ => {
+    console.log(db.iterator({ limit: -1 }).collect());
+  });
+
+  // Add an entry
+  const hash = await db.put("hello", "world");
+  console.log(hash);
+
+  // Query
+  console.log(db.get("hello"));
+});
+
+
 import fastify from 'fastify'
 
-async function main () {
-  const ipfsOptions = { repo : './ipfs', }
-  const ipfs = await IPFS.create(ipfsOptions)
-
-  const orbitdb = await OrbitDB.createInstance(ipfs)
-
-  const db = await orbitdb.keyvalue('first-database')
-
-  console.log(`db addr : ${db.address.toString()}`)
-}
-
-main()
 const server = fastify()
 
 server.get('/ping', async (_req, _res) => {
   return 'pong\n'
 })
 
-server.listen(8080, (err, address) => {
+server.listen(4242, (err, address) => {
   if (err) {
     console.error(err)
     process.exit(1)
